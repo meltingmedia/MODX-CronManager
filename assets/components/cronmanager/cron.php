cedfb25163a6e0fd3ebfe1f91d4984fafa1410a0
Romain <romain@melting-media.com>
2013-03-25 21:58:55 +0100

    Fix issue where newly added cron were executed even if not active

--- assets/components/cronmanager/cron.php
+++ assets/components/cronmanager/cron.php
@@ -13,48 +13,55 @@ $modx->lexicon->load('cronmanager:default');
 
 $rundatetime = date('Y-m-d H:i:s');
 
-// get all cronjobs wich needs to be runned
-$cronjobs = $modx->getCollection('modCronjob', array(
-	'nextrun' => null,
-	'OR:nextrun:<=' => $rundatetime,
-	'active' => true,
+$c = $modx->newQuery('modCronjob');
+$c->where(array(
+    array(
+        array('nextrun' => null,),
+        array('OR:nextrun:<=' => $rundatetime,),
+    ),
+    array(
+        'active' => true,
+    ),
 ));
+
+// get all cronjobs wich needs to be runned
+$cronjobs = $modx->getCollection('modCronjob', $c);
 /** @var modCronjob $cronjob */
 foreach($cronjobs as $cronjob) {
 
-	$properties = $cronjob->get('properties');
-	
-	if(!empty($properties)) {
-	
-		// try to get a propertyset
+    $properties = $cronjob->get('properties');
+
+    if (!empty($properties)) {
+
+        // try to get a propertyset
         /** @var modPropertySet $propset */
-		$propset = $modx->getObject('modPropertySet', array('name' => $properties));
-		if(!empty($propset) && is_object($propset) && $propset instanceof modPropertySet) {
-			$properties = $propset->getProperties();
-		}
-		// try if it is a json object
-		else if(substr($properties, 0, 1) == '{' && substr($properties, (strlen($properties)-1), 1) == '}') {
-			$props = $modx->fromJSON($properties);
-			if(!empty($props) && is_array($props)) {
-				$properties = $props;
-			}
-		}
-		// then must be it a key value pair group
-		else {
-			$lines = explode("\n", $properties);
-			$properties = array();
-			foreach($lines as $line) {
-				list($key, $value) = explode(':', $line);
-				$properties[trim($key)] = trim($value);
-			}
-		}
-	}
-	// when empty, make it an array
-	else {
-		$properties = array();
-	}
-	/** @var modSnippet $snippet */
-	$snippet = $cronjob->getOne('Snippet');
+        $propset = $modx->getObject('modPropertySet', array('name' => $properties));
+        if (!empty($propset) && is_object($propset) && $propset instanceof modPropertySet) {
+            $properties = $propset->getProperties();
+        }
+        // try if it is a json object
+        else if(substr($properties, 0, 1) == '{' && substr($properties, (strlen($properties)-1), 1) == '}') {
+            $props = $modx->fromJSON($properties);
+            if(!empty($props) && is_array($props)) {
+                $properties = $props;
+            }
+        }
+        // then must be it a key value pair group
+        else {
+            $lines = explode("\n", $properties);
+            $properties = array();
+            foreach($lines as $line) {
+                list($key, $value) = explode(':', $line);
+                $properties[trim($key)] = trim($value);
+            }
+        }
+    }
+    // when empty, make it an array
+    else {
+        $properties = array();
+    }
+    /** @var modSnippet $snippet */
+    $snippet = $cronjob->getOne('Snippet');
     /**
      * The snippet should return a json array :
      * array('error' => boolean, 'message' => string)
@@ -62,8 +69,8 @@ foreach($cronjobs as $cronjob) {
      *
      * This will allow to define if an error occurred and ease the process of filtering logs
      */
-	$response = $snippet->process($properties);
-    if(substr($response, 0, 1) == '{' && substr($response, (strlen($response)-1), 1) == '}') {
+    $response = $snippet->process($properties);
+    if (substr($response, 0, 1) == '{' && substr($response, (strlen($response)-1), 1) == '}') {
         $response = json_decode($response, true);
     } else {
         $msg = $response;
@@ -71,18 +78,16 @@ foreach($cronjobs as $cronjob) {
         $response['message'] = $msg;
     }
 
-	// add log run
-	$logs = array();
+    // add log run
+    $logs = array();
     /** @var modCronjobLog $log */
-	$log = $modx->newObject('modCronjobLog');
-	$log->fromArray($response);
-	$log->set('logdate', $rundatetime);
-	$logs[] = $log;
-	
-	$cronjob->set('lastrun', $rundatetime);
-	$cronjob->set('nextrun', date('Y-m-d H:i:s', (strtotime($rundatetime)+($cronjob->get('minutes')*60))));
-	$cronjob->addMany($logs);
-	$cronjob->save();
-}
+    $log = $modx->newObject('modCronjobLog');
+    $log->fromArray($response);
+    $log->set('logdate', $rundatetime);
+    $logs[] = $log;
 
-?>
+    $cronjob->set('lastrun', $rundatetime);
+    $cronjob->set('nextrun', date('Y-m-d H:i:s', (strtotime($rundatetime)+($cronjob->get('minutes')*60))));
+    $cronjob->addMany($logs);
+    $cronjob->save();
+}
