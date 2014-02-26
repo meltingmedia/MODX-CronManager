320dad12d4f780cc9876ba909b372953bc3a3415
Romain <romain@melting-media.com>
2014-02-26 16:39:47 +0100

    .

--- assets/components/cronmanager/cron.php
+++ assets/components/cronmanager/cron.php
@@ -2,6 +2,9 @@
 /**
  * @var modX $modx
  */
+
+set_time_limit(0);
+
 require_once dirname(dirname(dirname(dirname(__FILE__)))).'/config.core.php';
 require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
 require_once MODX_CONNECTORS_PATH.'index.php';
--- core/components/cronmanager/processors/mgr/cronjobs/process.class.php
+++ core/components/cronmanager/processors/mgr/cronjobs/process.class.php
@@ -1,5 +1,7 @@
 <?php
 
+set_time_limit(0);
+
 class RunNow extends modProcessor
 {
     /** @var modCronjob */
