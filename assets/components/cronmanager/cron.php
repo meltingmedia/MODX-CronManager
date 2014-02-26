cc5186dd198a493a1668057364c19d61c67e090b
Romain <romain@melting-media.com>
2014-02-26 17:22:40 +0100

    Coding style + make use of modX getService

--- assets/components/cronmanager/cron.php
+++ assets/components/cronmanager/cron.php
@@ -9,9 +9,11 @@ require_once dirname(dirname(dirname(dirname(__FILE__)))).'/config.core.php';
 require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
 require_once MODX_CONNECTORS_PATH.'index.php';
 
-$corePath = $modx->getOption('cronmanager.core_path',null,$modx->getOption('core_path').'components/cronmanager/');
-require_once $corePath.'model/cronmanager/cronmanager.class.php';
-$modx->cronmanager = new CronManager($modx);
+$corePath = $modx->getOption('cronmanager.core_path', null, $modx->getOption('core_path') . 'components/cronmanager/');
+//require_once $corePath.'model/cronmanager/cronmanager.class.php';
+
+/** @var CronManager $cm */
+$cm = $modx->getService('cronmanager', 'model.cronmanager.CronManager', $corePath);
 $modx->lexicon->load('cronmanager:default');
 
 $rundatetime = date('Y-m-d H:i:s');
@@ -27,49 +29,48 @@ $c->where(array(
     ),
 ));
 
-// get all cronjobs wich needs to be runned
+// get all cronjobs which needs to run
 $cronjobs = $modx->getCollection('modCronjob', $c);
 
 /** @type modCronjob $cronjob */
-foreach($cronjobs as $cronjob) {
-    $cronjob->set('nextrun', date('Y-m-d H:i:s', (strtotime($rundatetime)+($cronjob->get('minutes')*60))));
+foreach ($cronjobs as $cronjob) {
+    $nextRun = (strtotime($rundatetime) + ( $cronjob->get('minutes') * 60 ) );
+    $cronjob->set('nextrun', date('Y-m-d H:i:s', $nextRun));
     $cronjob->save();
 }
 
 /** @type modCronjob $cronjob */
-foreach($cronjobs as $cronjob) {
+foreach ($cronjobs as $cronjob) {
 
     $properties = $cronjob->get('properties');
 
     if (!empty($properties)) {
-
-        // try to get a propertyset
+        // try to get a property set
         /** @var modPropertySet $propset */
         $propset = $modx->getObject('modPropertySet', array('name' => $properties));
+
         if (!empty($propset) && is_object($propset) && $propset instanceof modPropertySet) {
             $properties = $propset->getProperties();
-        }
-        // try if it is a json object
-        else if(substr($properties, 0, 1) == '{' && substr($properties, (strlen($properties)-1), 1) == '}') {
+        } elseif (substr($properties, 0, 1) == '{' && substr($properties, (strlen($properties)-1), 1) == '}') {
+            // try if it is a json object
             $props = $modx->fromJSON($properties);
-            if(!empty($props) && is_array($props)) {
+            if (!empty($props) && is_array($props)) {
                 $properties = $props;
             }
-        }
-        // then must be it a key value pair group
-        else {
+        } else {
+            // then must be it a key value pair group
             $lines = explode("\n", $properties);
             $properties = array();
-            foreach($lines as $line) {
+            foreach ($lines as $line) {
                 list($key, $value) = explode(':', $line);
                 $properties[trim($key)] = trim($value);
             }
         }
-    }
-    // when empty, make it an array
-    else {
+    } else {
+        // when empty, make it an array
         $properties = array();
     }
+
     /** @var modSnippet $snippet */
     $snippet = $cronjob->getOne('Snippet');
     /**
